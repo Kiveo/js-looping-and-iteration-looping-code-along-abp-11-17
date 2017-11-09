ed616f4b4c3d794ad5f96bed2618246b2f33e21a
Maverick Tesini <mavericktesini@gmail.com>
2017-11-09 23:58:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+function printBadges(string) {
+  const employees = ['Joe', 'Gabe', 'Lisa'];
+  var elength = employees.length;
+  for (i=0; i < elength; i++) {
+    console.log('Welcome ' + employees[i] + '!' + ' You are employee #' + (i+1) + '.');
+  }
+  return string;
+}
+
+function flip() {
+  return Math.random() >= 0.5;
+}
+function tailsNeverFails() {
+  var x = 0;
+  while (flip() ) {
+    ++x;
+  }
+  return console.log('You got ' + x + ' tails in a row!');
+}
