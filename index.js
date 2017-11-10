ed17616d7fb50760686c73910aa1fd57bb170fc5
Maverick Tesini <mavericktesini@gmail.com>
2017-11-10 00:05:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ function printBadges(string) {
   return string;
 }
 
-var x = 0;
 function flip() {
-  ++x;
   return Math.random() >= 0.5;
 }
 
 function tailsNeverFails() {
+  var x = 0;
   while (flip() ) {
+    ++x;
+  }
   return console.log('You got ' + x + ' tails in a row!');
-}
 }
